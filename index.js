c1ea633f8113782e6221c857142a588f184f8e7c
mianmalife <253590433@qq.com>
2020-11-21 23:46:03 +0800

    'add:boostrap_temp'

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const serve = require('koa-static')
+const static = require('koa-static')
 const path = require('path')
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
@@ -8,13 +8,13 @@ const app = new Koa()
 
 app.use(bodyParser())
 
-const render = views(__dirname + '/view', { extension: 'ejs' })
+const render = views(__dirname + '/view', { map: { html: 'ejs' } })
 
 app.use(render)
 
 app.use(registerRouter())
 
-app.use(serve(path.join(__dirname + '/public')))
+app.use(static(path.join(__dirname + '/public')))
 
 app.listen(8000, () => {
     console.log('server is running')
